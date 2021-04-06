c0be1879f9adf551331abc7bd84cfb814ba88b13
Adam Jonas <adamjonas25@gmail.com>
2021-04-06 19:41:04 +0100

    Give transactions a date

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -1,4 +1,10 @@
-const assert = require('assert')
+const assert = require('assert');
+const MockDate = require('mockdate');
+const date = new Date;
+
+before(() => {
+  MockDate.set(date)
+});
 
 describe('Transaction', () => {
   const Transaction = require('../lib/transaction');
@@ -7,6 +13,14 @@ describe('Transaction', () => {
     it('returns the amount of the transaction', () => {
       const transaction = new Transaction(100)
       assert.strictEqual(transaction.amount, 100)
+    });
+  });
+
+  describe('#date', () => {
+    it('returns the date of the transaction', () => {
+      const transaction = new Transaction(100)
+
+      assert(transaction.date === date.toLocaleDateString)
     });
   });
 });
