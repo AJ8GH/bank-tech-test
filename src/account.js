6587758ad21409bb7d7f235b1494a9ef980dc564
Adam Jonas <adamjonas25@gmail.com>
2021-04-06 14:37:11 +0100

    Implement deposit function

--- src/account.js
+++ src/account.js
@@ -2,6 +2,10 @@ class Account {
   constructor() {
     this.balance = 0
   }
+
+  deposit() {
+    this.balance += 100
+  }
 }
 
 module.exports = Account;
--- tests/account.test.js
+++ tests/account.test.js
@@ -1,10 +1,22 @@
 const Account = require('../src/account');
 
 describe('Account', () => {
+  let account;
+
+  beforeEach(() => {
+    account = new Account();
+  });
+
   describe('balance()', () => {
     it('is 0 by default', () => {
-      account = new Account();
       expect(account.balance).toBe(0);
+    });
+  });
+
+  describe('deposit()', () => {
+    it('adds amount to balance', () => {
+      account.deposit(100);
+      expect(account.balance).toBe(100)
     });
   });
 });
