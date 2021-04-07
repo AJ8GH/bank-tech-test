fe7f8bf8a84b7cb2aeab87957132517c048121c0
Adam Jonas <adamjonas25@gmail.com>
2021-04-07 14:39:00 +0100

    Implement printing multiple statements

--- test/feature.test.js
+++ test/feature.test.js
@@ -40,4 +40,28 @@ describe('Feature testing', () => {
     account.printStatement()
     sinon.assert.calledWith(console.log, output)
   })
+
+  it('prints multiple statements correctly', () => {
+    const output1 = [
+      'date || credit || debit || balance',
+      '01/02/2021 || £500.00 ||  || £500.00',
+      '01/02/2021 ||  || £49.99 || £450.01'
+    ].join("\n")
+
+    const output2 = [
+      'date || credit || debit || balance',
+      '01/02/2021 || £500.00 ||  || £500.00',
+      '01/02/2021 ||  || £49.99 || £450.01',
+      '01/02/2021 ||  || £101.05 || £348.96'
+    ].join("\n")
+
+    account.deposit(500)
+    account.withdraw(49.99)
+    account.printStatement()
+    sinon.assert.calledWith(console.log, output1)
+
+    account.withdraw(101.05)
+    account.printStatement()
+    sinon.assert.calledWith(console.log, output2)
+  })
 })
