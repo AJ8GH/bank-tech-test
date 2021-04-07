e8a573e1608280c13d90a093ca7100a072e5f501
Adam Jonas <adamjonas25@gmail.com>
2021-04-07 09:27:36 +0100

    Refactor printing functions into printer class

--- test/account.test.js
+++ test/account.test.js
@@ -55,32 +55,6 @@ describe('Account', () => {
     });
   });
 
-  describe('#print_statement()', () => {
-    const spy = sinon.spy(console, 'log')
-
-    describe('with no transactions', () => {
-      it('prints the balance and date', () => {
-        const statement1 = "date | balance\n01/01/2021 | 0.00"
-        mockdate.set('01/01/2021')
-        account.printStatement()
-        assert(spy.calledWith(statement1));
-      });
-
-      it('knows the correct date', () => {
-        const statement1 = "date | balance\n02/02/2021 | 0.00"
-        mockdate.set('02/02/2021')
-        account.printStatement()
-        assert(spy.calledWith(statement1));
-      });
-    });
-
-    describe('with one transaction', () => {
-      it('prints table with one row', () => {
-        const statement1 = "date | balance\n01/01/2021 | 0.00"
-        mockdate.set('01/01/2021')
-        account.printStatement()
-        assert(spy.calledWith(statement1));
-      });
-    });
-  });
+  // describe('#print_statement()', () => {
+  // });
 });
---
+++ test/printer.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon')
+const assert = require('assert')
+const mockdate = require('mockdate');
+
+describe('Printer', () => {
+  const printer = require('../lib/printer')
+
+  describe('#print_statement()', () => {
+    let spy;
+
+    beforeEach(() => { spy = sinon.spy(console, 'log'); });
+    afterEach(() => { spy.restore(); });
+
+
+    describe('with no transactions', () => {
+      it('prints the balance and date', () => {
+        const statement1 = "date | balance\n01/01/2021 | 0.00"
+        mockdate.set('01/01/2021')
+        printer.printStatement(([]))
+        assert(spy.calledWith(statement1));
+      });
+
+      it('knows the correct date', () => {
+        const statement1 = "date | balance\n02/02/2021 | 0.00"
+        mockdate.set('02/02/2021')
+        printer.printStatement([])
+        assert(spy.calledWith(statement1));
+      });
+    });
+  });
+});
