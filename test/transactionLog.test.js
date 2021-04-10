eecd04b4293d3ae893afb749ca88f797921e5bfa
Adam Jonas <adamjonas25@gmail.com>
2021-04-10 20:46:47 +0100

    Move before each filter into describe block

--- test/transactionLog.test.js
+++ test/transactionLog.test.js
@@ -1,11 +1,13 @@
 const assert = require('assert')
 const TransactionLog = require('../lib/transactionLog')
 
-beforeEach(() => {
-  transactionLog = new TransactionLog()
-})
-
 describe('TransactionLog', () => {
+  let transactionLog
+
+  beforeEach(() => {
+    transactionLog = new TransactionLog()
+  })
+
   describe('#transactions()', () => {
     it('starts empty', () => {
       assert.strictEqual(transactionLog.transactions.length, 0)
